aa585b3026f9fa043f6b7609371be71bef6fe8f2
jefferson.damaceno17@gmail.com <jefferson.damaceno17@gmail.com>
2018-12-29 20:27:36 -0200

    adding a status Route to see server status

---
+++ backend/routes/statusRoutes.js
@@ -0,0 +1,12 @@
+// Initialize express router
+let router = require('express').Router();
+
+// Set default API response
+router.get('/status', function (req, res) {
+    res.json({
+        status: 'API Its Working',
+        message: 'Welcome to User-Register crafted with love!',
+    });
+});
+
+module.exports = router

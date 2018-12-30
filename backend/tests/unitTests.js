6fece626baea8234f6a1d5312aafdf2b504c32d5
jefferson.damaceno17@gmail.com <jefferson.damaceno17@gmail.com>
2018-12-30 19:19:54 -0200

    Refactoring unitTests to use chai requests

--- backend/tests/unitTests.js
+++ backend/tests/unitTests.js
@@ -1,77 +1,125 @@
-const test = require('tape')
-const request = require('supertest')
-const express = require('express')
+let mongoose = require("mongoose");
+let User = require('../models/userModel');
 
-const User = require('../models/user')
-const app = require('../index')
-let userId
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+let should = chai.should();
 
-before(done => {
-    app.on('APP_STARTED', () => {
-        done()
-    })
-})
+chai.use(chaiHttp);
 
-describe('API Integration Test', function() {
-    it('Runs all tests', function(done) {
-
-        test('/api/v1/users/', assert => {
-            request(app)
-                .post('/api/v1/users/')
-                .send(new User('test name', '27810384074'))
-                .expect(200)
+describe('API tests', () => {
+    beforeEach((done) => {
+        User.deleteOne({}, (err) => {
+            done();
+        });
+    });
+    describe('/GET user', () => {
+        it('it should GET all the users', (done) => {
+            chai.request(server)
+                .get('/api/v1/users')
                 .end((err, res) => {
-                    if (err) return assert.fail(JSON.stringify(res))
-                    assert.pass('Created a new user successfully, test passed!')
-                    assert.end()
-                })
-        })
-
-        test('/api/v1/users/', assert => {
-            request(app)
-                .get('/api/v1/users/')
-                .expect(200)
+                    res.should.have.status(200);
+                    res.body.should.have.property('message').eql('Users retrieved successfully');
+                    done();
+                });
+        });
+    });
+    describe('/POST user', () => {
+        it('it should POST a user ', (done) => {
+            let user = {
+                name: "Dummyname",
+                cpfcnpj: "80220128073"
+            }
+            chai.request(server)
+                .post('/api/v1/users')
+                .send(user)
                 .end((err, res) => {
-                    if (err) return assert.fail(JSON.stringify(res))
-                    userId = res.body[0]._id
-                    assert.pass('Got all users successfully, test passed!')
-                    assert.end()
-                })
-        })
-
-        test('/api/v1/users/:id', assert => {
-            request(app)
-                .get(`/api/v1/users/${userId}`)
-                .expect(200)
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message').eql('New user created!');
+                    res.body.data.should.have.property('name');
+                    res.body.data.should.have.property('cpfcnpj');
+                    done();
+                });
+        });
+        it('it should not POST a user', (done) => {
+            let user = {
+                name: "Jefferson Bruchado",
+                cpfcnpj: "70131172000"
+            }
+            chai.request(server)
+                .post('/api/v1/users')
+                .send(user)
                 .end((err, res) => {
-                    if (err) return assert.fail(JSON.stringify(res))
-                    assert.pass('Got a specific user successfully, test passed!')
-                    assert.end()
-                })
-        })
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+    });
+    describe('/GET/:id user', () => {
+        it('it should GET a user by the given id', (done) => {
+            let user = new User({
+                name: "Foobar",
+                cpfcnpj: "64562650028"
+            });
+            user.save((err, user) => {
+                chai.request(server)
+                    .get('/api/v1/users/' + user.id)
+                    .send(user)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.data.should.have.property('name');
+                        res.body.data.should.have.property('cpfcnpj');
+                        res.body.data.should.have.property('_id').eql(user.id);
+                        done();
+                    });
+            });
 
-        test('/api/v1/users/:id', assert => {
-            request(app)
-                .patch(`/api/v1/users/${userId}`)
-                .send(new User('test name edit', 'test cpfcnpj edit'))
-                .expect(200)
-                .end((err, res) => {
-                    if (err) return assert.fail(JSON.stringify(res))
-                    assert.pass('Edited a users successfully, test passed!')
-                    assert.end()
-                })
-        })
-
-        test('/api/v1/users/:id', assert => {
-            request(app)
-                .delete(`/api/v1/users/${userId}`)
-                .expect(200)
-                .end((err, res) => {
-                    if (err) return assert.fail(JSON.stringify(res))
-                    assert.pass('Deleted a specific user successfully, test passed!')
-                    assert.end()
-                    done()
-                })
-        })
-    })
-})
+        });
+    });
+    describe('/PUT/:id users', () => {
+        it('it should UPDATE a user given the id', (done) => {
+            let user = new User({
+                name: "Foobarr",
+                cpfcnpj: "13467309038"
+            });
+            let userEdited = new User({
+                name: "Foobaredited",
+                cpfcnpj: "89847716005"
+            });
+            user.save((err, user) => {
+                chai.request(server)
+                    .put('/api/v1/users/' + user.id)
+                    .send(userEdited)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.have.property('message').eql('User Info updated');
+                        res.body.data.should.have.property('name').eql('Foobaredited');
+                        res.body.data.should.have.property('cpfcnpj').eql('89847716005');
+                        done();
+                    });
+            });
+        });
+    });
+    describe('/DELETE/:id user', () => {
+        it('it should DELETE a user given the id', (done) => {
+            let user = new User({
+                name: "Dummyname",
+                cpfcnpj: "42643960000110"
+            })
+            user.save((err, user) => {
+                chai.request(server)
+                    .delete('/api/v1/users/' + user.id)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('message').eql('User deleted');
+                        done();
+                    });
+            });
+        });
+    });
+});

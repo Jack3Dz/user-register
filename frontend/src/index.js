c939d7aab33291807f0ae7923ea2d842cb8ef4a0
jefferson.damaceno17@gmail.com <jefferson.damaceno17@gmail.com>
2019-01-04 10:52:07 -0200

    installing packages and adding redux store to the application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,19 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Store } from './store'
+import { Provider } from 'react-redux';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { fetchAllUsers } from './actions/user';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+Store.dispatch(fetchAllUsers());
+
+ReactDOM.render(
+    <Provider store={Store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+
+registerServiceWorker();

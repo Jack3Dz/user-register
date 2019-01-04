841958ae91308cdb3a0d519d8ac6f3d3d2c63bec
jefferson.damaceno17@gmail.com <jefferson.damaceno17@gmail.com>
2019-01-04 11:06:51 -0200

    configuring app.js to use CreateUser and UserList component and added a ReduxToastr to user actions messaging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CreateUser from './containers/CreateUser';
+import UserList from './containers/UserList';
+import ReduxToastr from 'react-redux-toastr'
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
+import './css/main.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div>
+          <ReduxToastr
+            timeOut={4000}
+            newestOnTop={false}
+            preventDuplicates
+            position="top-right"
+            transitionIn="fadeIn"
+            transitionOut="fadeOut"
+            progressBar
+            closeOnToastrClick />
+        </div>
+        <div className="container">
+          <h1 className="styles-app">Usuários</h1>
+          <div className="row styles-app">
+            <div className="col-md-12">
+              <CreateUser />
+            </div>
+            <div className="col-md-12">
+              <UserList />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
